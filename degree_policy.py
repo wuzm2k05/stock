fad1354b688bf76a40c94b32c6de77a7ec42c1ab
wuzhaoming <wuzm2k05@hotmail.com>
2024-10-24 11:46:03 +0800

    report error information

--- degree_policy.py
+++ degree_policy.py
@@ -6,6 +6,7 @@ import core.snowball_proxy as snowball_proxy
 import core.logger as logger
 import core.trade_time as trade_time
 import core.report as report
+import core.email as email
 
 _log = logger.get_logger()
 
@@ -18,21 +19,29 @@ def check_stock_config_attr(stock_name,stock_attr):
     raise Exception(" %s stock_attr check fail",stock_name)
 
 async def main():
-  try:
-    config.output_configs(_log)
-    min_trade_stocks = config.get_policy_min_trade_stocks()
-    stocks = json.loads(config.get_policy_stocks_config())
-    for stock_name, stock_attr in stocks.items():
-      check_stock_config_attr(stock_name,stock_attr)
-      # fill the min trade stocks for those stocks which not configured in trade stocks
-      if "min_trade_stocks" not in stock_attr:
-        stock_attr["min_trade_stocks"] = min_trade_stocks
+  
+  config.output_configs(_log)
+  min_trade_stocks = config.get_policy_min_trade_stocks()
+  stocks = json.loads(config.get_policy_stocks_config())
+  for stock_name, stock_attr in stocks.items():
+    check_stock_config_attr(stock_name,stock_attr)
+    # fill the min trade stocks for those stocks which not configured in trade stocks
+    if "min_trade_stocks" not in stock_attr:
+      stock_attr["min_trade_stocks"] = min_trade_stocks
     
-    proxy = snowball_proxy.SnowBallProxy()
-    policy = degree_policy.DegreePolicy()
-    
-    while True:
+  proxy = snowball_proxy.SnowBallProxy()
+  policy = degree_policy.DegreePolicy()
+  error_info = None
+  
+  while True:
+    try:  
       await asyncio.sleep(config.get_policy_check_interval_in_seconds())
+      
+      if error_info:
+        # try send error information last time
+        email.send_email("Internal Error",error_info)
+        error_info = None
+        
       _log.debug("one cycle")
       report.trigger_report()
       # first check if it is in non-trade time
@@ -47,17 +56,18 @@ async def main():
       
       #check if responses are okay, otherwise skip this time
       if balance.result_code != "60000" or order_list.result_code != "60000" or position_list.result_code != "60000":
-        _log.warn("proxy get balance,order_list,position_list fail")
+        _log.warning("proxy get balance,order_list,position_list fail")
         continue
       
       #then for each stock, run policy
       for stock_name, stock_attr in stocks.items():
         policy.run_policy_for_stock(stock_name,stock_attr,balance,order_list,position_list)
-      
-  except Exception as e:
-    # any uncaughted exception will make program exit
-    traceback_str = traceback.format_exc()
-    _log.warning("internal error: %s", traceback_str)
+    
+    except Exception as e:
+      # any uncaughted exception will make program exit
+      traceback_str = traceback.format_exc()
+      _log.warning("internal error: %s", traceback_str)
+      error_info = traceback_str
     
 # Start servers
 if __name__ == "__main__":
